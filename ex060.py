dfadd9950da0799c886d0d3973ab256ddcbda09e
unknown <justinmumu@gmail.com>
2020-12-11 07:13:43 +0900

    added python file

---
+++ ex060.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# nums라는 리스트를 만든다.
+nums = [1, 2, 3, 4, 5]
+# average의 값을 'sum 함수를 사용한 리스트내의 모든 원소들의 합 / len 함수를 사용한 리스트내의 모든 원소들의 갯수'로 설정한다.
+average = sum(nums) / len(nums)
+# average값을 출력한다.
+print(average)
+
+# 실행결과 : 3.0
+
